4231025ab8c980beb785d72cbe8f24aa3add1f02
Armando Bosso <bossoar@gmail.com>
2015-06-04 20:44:07 -0300

    Agrego Repositorio Marca, agrego campo a clase marca

--- dom/src/main/java/domainapp/dom/modules/atencion/Marca.java
+++ dom/src/main/java/domainapp/dom/modules/atencion/Marca.java
@@ -1,5 +1,67 @@
 package domainapp.dom.modules.atencion;
 
-public class Marca {
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.VersionStrategy;
 
+import org.apache.isis.applib.DomainObjectContainer;
+import org.apache.isis.applib.annotation.BookmarkPolicy;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.MemberOrder;
+
+@javax.jdo.annotations.PersistenceCapable(identityType=IdentityType.DATASTORE)
+
+@javax.jdo.annotations.DatastoreIdentity(
+        strategy=javax.jdo.annotations.IdGeneratorStrategy.IDENTITY,
+         column="id")
+@javax.jdo.annotations.Version(
+        strategy=VersionStrategy.VERSION_NUMBER, 
+        column="version")
+@javax.jdo.annotations.Queries({
+        @javax.jdo.annotations.Query(
+                name = "find", language = "JDOQL",
+                value = "SELECT "
+                        + "FROM domainapp.dom.modules.atencion.Marca "),
+        @javax.jdo.annotations.Query(name = "findByDescripcion", language = "JDOQL", value = "SELECT "
+                    			+ "FROM dom.modules.atencion.Descripcion " + "WHERE descripcion == :descripcion"),
+    
+})
+
+@DomainObject(
+        objectType = "MARCA"
+)
+@DomainObjectLayout(
+        bookmarking = BookmarkPolicy.AS_ROOT
+)
+
+public class Marca {
+	
+	public DomainObjectContainer getContainer() {
+		return container;
+	}
+
+	public void setContainer(DomainObjectContainer container) {
+		this.container = container;
+	}
+	//
+		  
+    
+    private String descripcion;
+    
+    @Persistent
+	@MemberOrder(sequence = "1")
+    @javax.jdo.annotations.Column(allowsNull="false", length = 40)
+	
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(final String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    @javax.inject.Inject
+
+	private DomainObjectContainer container;
 }
