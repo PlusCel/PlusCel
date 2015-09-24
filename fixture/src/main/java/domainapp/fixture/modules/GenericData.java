46d5dfe6381bfc57787ecf13ff92ed07b55ce04f
Armando Bosso <bossoar@gmail.com>
2015-09-24 00:06:02 -0300

    agrego fixture cliente

---
+++ fixture/src/main/java/domainapp/fixture/modules/GenericData.java
@@ -0,0 +1,93 @@
+/*
+ * This is a software made for highschool management 
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package domainapp.fixture.modules;
+
+import java.util.Random;
+
+public class GenericData {
+	
+	private static int	Cantidad=20;
+	
+	private static String Nombre="Santiago,Sebastián,Matías,Nicolás,Samuel,Alejandro,Mateo,Diego,Benjamín,Daniel,Joaquín,Tomás,Gabriel,Lucas,Martín,Emmanuel,Alexander,David,Emiliano,Carlos,JuanJosé,Andrés,Felipe,Ignacio,Leonardo,Adrián,Francisco,Rodrigo,Alvaro,MiguelÁngel,Fernando,Santino,Bautista,Agustín,JuanPablo,Vicente,Thiago,Maximiliano,Pablo,Eduardo,Christopher,Kevin,Isaac,JuanDiego,Aarón,Dylan,Jesús,Esteban,Manuel,JuanSebastián,Franco,Lautaro,Miguel,JuanDavid,Ricardo,Bruno,Luciano,Juan,Emilio,JuanEsteban,Julián,Valentino,Javier,Joshua,Rafael,Jorge,José,Luis,DiegoAlejandro,Gael,Óscar,Nahuel,Máximo,Axel,Facundo,Jonathan,Ian,Josué,Camilo,Sergio,Jerónimo,Álex,Mauricio,JuanCamilo,Alonso,Anthony,Dante,Christian,Simón,Patricio,Héctor,Iván,Marcos,Ramiro,Alberto,Matthew,Pedro,Mario,Alan,Arturo,Adrian,Manuel,Jose,Gogo,Pedro,Mariano,Leandro,Gonzalo,Roberto,Daniel,Fernando,Damian,Oscar,Lautaro,Miguel,Diego,Pablo,Raul,Ricardo,Matias,Hector,Juan,Emiliano,Cesar,Gerardo,Emiliano,Francisco,Martin,Leonardo";
+	private static String Apellido="Lala,González,Rodríguez,Lolo,Gómez,Fernández,López,Torroija,Díaz,Martínez,Pérez,García,Sánchez,Romero,Sosa,Álvarez,Torres,Ruiz,Ramírez,Flores,Acosta,Benítez,Medina,Suárez,Herrera,Aguirre,Pereyra,Gutiérrez,Giménez,Molina,Silva,Benigar,Castro,Rojas,Ortíz,Núñez,Luna,Juárez,Cabrera,Ríos,Ferreyra,Godoy,Morales,Domínguez,Moreno,Peralta,Vega,Carrizo,Quiroga,Garcia Pacek,Castillo,Ledesma,Muñoz,Ojeda,Ponce,Vera,Vázque,Villalba,Cardozo,Navarro,Ramos,Arias,Coronel,Córdoba,Figueroa,Correa,Cáceres,Vargas,Maldonado,Mansilla,Farías,Rivero,Paz,Miranda,Roldán,Méndez,Lucero,Cruz,Hernández,Agüero,Páez,Blanco,Mendoza,Barrios,Escobar,Ávila,Soria,Leiva,Acuña,Martin,Maidana,Moyano,Cola,Campos,Olivera,Duarte,Soto,Franco,Bravo,Valdéz,Toledo,Andrada,Andrade,Montenegro,Leguizamón,Chávez,Arce,Valdez,Yong Wong";
+	private static String Materias="Lengua,Historia,Fisica,Matematica,Civica,Biologia,Geografia,Quimica,Analisis Matematico,Contabilidad,Musica,Plastica,Lengua y literatura,Dibujo,Educacion fisica,Taller";
+	private static String Calle="Jujuy,La Rioja,Salta,Pampa,Misiones,Buenos Ahires,Bahia Blanca,Mendoza,Santafe,Boedo,San Martin,Belgrano,Aconcagua,Domene,Chile,España,Godoy,Perito Moreno";
+	private static String Letras="A,B,C,D,E,F,G,H";
+	private static String Plan="Bachillerato,Tecnico Maestro mayor de obra,Musica,Agricola,Artistica,Polimodal,Tecnico Industrial,Tecnico Auxiliar,Tecnico Mecanico,Tecnico Quimico,Tecnico Electromecanico,Bachillerato Perito Mercantil,Bachillerato Contable";
+//	
+	public static int ObtenerCantidad()
+	{
+		return Cantidad;
+	}
+	
+	
+//	public static String ObtenerPlan()
+//	{
+//		return ObtenerValor(Plan);
+//	}
+//	
+	
+	public static String ObtenerNombre()
+	{
+		return ObtenerValor(Nombre);
+	}
+	
+	public static String ObtenerApellido()
+	{
+		return ObtenerValor(Apellido);
+	}
+	
+	public static String ObtenerCiencia()
+	{
+		return ObtenerValor(Materias);
+	}
+	
+	public static String ObtenerCalle()
+	{
+		return ObtenerValor(Calle);
+	}
+	
+	public static String ObtenerLetras(int index)
+	{
+		String[] partes=Dividir(Letras);
+		return partes[index];
+	}
+	
+	public static int Random(int Inicial, int Final)
+	{
+		Random rnd = new Random();
+		int Resul=(int) (rnd.nextDouble() * Final + Inicial);
+		return Resul;
+	}
+	
+	private static String ObtenerValor(String valor)
+	{
+		String[] Partes=Dividir(valor);
+		return Partes[Random(0,Partes.length)];
+	}
+	
+	public static String[] Dividir(String palabras)
+	{
+		String[] partes = palabras.split(",");
+		return partes;
+	}
+	
+	
+}
---
+++ fixture/src/main/java/domainapp/fixture/scenarios/ClientesFixture.java
@@ -0,0 +1,110 @@
+/*
+ * This is a software made for highschool management 
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package domainapp.fixture.scenarios;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domainapp.dom.modules.atencion.Cliente;
+import domainapp.dom.modules.atencion.ClienteRepositorio;
+import domainapp.dom.modules.servicios.E_nacionalidad;
+import domainapp.dom.modules.servicios.E_sexo;
+import domainapp.dom.modules.servicios.Localidad.E_localidades;
+import domainapp.fixture.modules.GenericData;
+
+
+import domainapp.fixture.modules.MarcaTearDown;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.joda.time.LocalDate;
+
+public class ClientesFixture extends FixtureScript {
+
+    public ClientesFixture() {
+        withDiscoverability(Discoverability.DISCOVERABLE);
+    }
+
+    @Override
+    protected void execute(ExecutionContext executionContext) {
+
+        // prereqs
+    	BorrarDBClientes(executionContext);
+        
+        int Cantidad=GenericData.ObtenerCantidad()*14;
+        
+        List<Cliente> listAl=new ArrayList<Cliente>();
+        
+        // create
+        for(int x=0; x<=Cantidad;x++)
+        {
+        	Cliente al=new Cliente();
+        	al.setNombre(GenericData.ObtenerNombre());
+        	al.setApellido(GenericData.ObtenerApellido());
+        	
+        	listAl.add(al);
+        	
+        }
+        for(Cliente al:removerrepetidos(listAl))
+        create(al.getApellido(),al.getNombre(),GenericData.Random(40000000, 60000000),E_sexo.MASCULINO,LocalDate.now(),E_nacionalidad.ARGENTINA, E_localidades.NEUQUEN,GenericData.ObtenerCalle(), GenericData.Random(1, 9999),null,null,null,String.valueOf(GenericData.Random(10000000, 88888888)), executionContext);
+    }
+
+    // //////////////////////////////////////
+
+	private List<Cliente> removerrepetidos(List<Cliente> listaCliente)
+	{
+		
+		for(int x=0;x<listaCliente.size()-1;x++)
+		{
+				for(int y=x+1;y<listaCliente.size();y++)
+				{
+					
+					if(listaCliente.get(x).getNombre().equals(listaCliente.get(y).getNombre()) && listaCliente.get(x).getApellido().equals(listaCliente.get(y).getApellido()))
+					{
+						listaCliente.remove(y);
+					}
+					
+				}
+		}
+		
+		return listaCliente;
+	}
+    
+    @SuppressWarnings("deprecation")
+	private Cliente create(final String apellido, String nombre,int dni,E_sexo sexo,LocalDate nacimiento,E_nacionalidad nacionalidad, E_localidades localidad, String calle, int numero, String piso,String departamento,String  email,String telefono, ExecutionContext executionContext) {
+        return executionContext.add(this, Clientes.altaCliente(apellido, nombre, dni, sexo, nacimiento, nacionalidad, localidad, calle, numero, piso, departamento,email, telefono));
+    }
+
+    // //////////////////////////////////////
+
+    @SuppressWarnings("deprecation")
+	public void BorrarDBClientes(ExecutionContext executionContext)
+    {
+        execute(new MarcaTearDown(), executionContext);
+    	
+       return;
+    }
+    
+    
+    @javax.inject.Inject
+    private ClienteRepositorio Clientes;
+//    @javax.inject.Inject
+//    private IsisJdoSupport isisJdoSupport; 
+
+}
