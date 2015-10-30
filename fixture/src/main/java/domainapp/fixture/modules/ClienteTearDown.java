593cedfa3f53e8c4f71a0823846d209f8ffc0ff6
Armando Bosso <bossoar@gmail.com>
2015-10-30 21:38:50 -0200

    modifico borrado de tabla clientes

---
+++ fixture/src/main/java/domainapp/fixture/modules/ClienteTearDown.java
@@ -0,0 +1,18 @@
+package domainapp.fixture.modules;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.fixturescripts.FixtureScript.ExecutionContext;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+
+
+public class ClienteTearDown extends FixtureScript {
+	
+	 @Override
+	    protected void execute(ExecutionContext executionContext) {
+	        isisJdoSupport.executeUpdate("delete from \"Cliente\"");
+	    }
+
+	    @javax.inject.Inject
+	    private IsisJdoSupport isisJdoSupport;
+
+}
