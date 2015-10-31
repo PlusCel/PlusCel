f7992c86a5aea90fed35e46c6a9c44000762609c
Armando Bosso <bossoar@gmail.com>
2015-10-31 00:36:40 -0200

    Agrego la clase para eliminar los datos de tipo de falla

---
+++ fixture/src/main/java/domainapp/fixture/modules/TipoFallaTearDown.java
@@ -0,0 +1,18 @@
+package domainapp.fixture.modules;
+
+import org.apache.isis.applib.fixturescripts.FixtureScript;
+import org.apache.isis.applib.fixturescripts.FixtureScript.ExecutionContext;
+import org.apache.isis.applib.services.jdosupport.IsisJdoSupport;
+
+public class TipoFallaTearDown  extends FixtureScript {
+
+    @Override
+    protected void execute(ExecutionContext executionContext) {
+        isisJdoSupport.executeUpdate("delete from \"TipoFalla\"");
+    }
+
+    @javax.inject.Inject
+    private IsisJdoSupport isisJdoSupport;
+
+
+}
