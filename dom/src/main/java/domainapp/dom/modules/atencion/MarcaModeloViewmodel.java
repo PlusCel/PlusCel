5e0d5465e8813fa0e1faa2106ce99e831adbbcfb
nachocartes <nachocartes@gmail.com>
2015-11-15 14:56:06 -0300

    Agrego accion carga rapida cliente

--- dom/src/main/java/domainapp/dom/modules/atencion/MarcaModeloViewmodel.java
+++ dom/src/main/java/domainapp/dom/modules/atencion/MarcaModeloViewmodel.java
@@ -10,6 +10,8 @@ import org.apache.isis.applib.annotation.CollectionLayout;
 import org.apache.isis.applib.annotation.Editing;
 import org.apache.isis.applib.annotation.MemberGroupLayout;
 import org.apache.isis.applib.annotation.MemberOrder;
+import org.apache.isis.applib.annotation.Parameter;
+import org.apache.isis.applib.annotation.ParameterLayout;
 import org.apache.isis.applib.annotation.Programmatic;
 import org.apache.isis.applib.annotation.RenderType;
 import org.apache.isis.applib.services.memento.MementoService;
@@ -23,6 +25,7 @@ import domainapp.dom.modules.atencion.Modelo;
 import domainapp.dom.modules.atencion.ModeloRepositorio;
 import domainapp.dom.modules.atencion.OrdenServicio;
 import domainapp.dom.modules.atencion.OrdenServicioRepositorio;
+
 
 @MemberGroupLayout(columnSpans = { 4, 0, 0, 8 })
 public class MarcaModeloViewmodel extends AbstractViewModel {
@@ -42,7 +45,27 @@ public class MarcaModeloViewmodel extends AbstractViewModel {
 	public void setMarca(final String marca) {
 		this.marca = marca;
 	}
-	// }}
+	// Agrego cliente datos basicos
+	
+	@MemberOrder(sequence = "1")
+    public Cliente altaRapidaCliente(
+            final  @ParameterLayout(named="Apellido") @Parameter(regexPattern = domainapp.dom.modules.validador.ValidadorCaracteres.ValidacionLetras.PERMITIDOS, maxLength = 40) String apellido,
+            final @ParameterLayout(named="Nombre") @Parameter(regexPattern = domainapp.dom.modules.validador.ValidadorCaracteres.ValidacionLetras.PERMITIDOS, maxLength = 40)  String nombre,
+            final @ParameterLayout(named="Dni")@Parameter(regexPattern = domainapp.dom.modules.validador.ValidadorCaracteres.ValidacionNumerica.PERMITIDOS, maxLength = 12)  int dni,
+            final @ParameterLayout(named="Teléfono") @Parameter(regexPattern = domainapp.dom.modules.validador.ValidadorCaracteres.ValidacionTel.PERMITIDOS, maxLength = 30) String telefono
+            
+    		) {
+        final Cliente obj = container.newTransientInstance(Cliente.class);
+        
+        obj.setNombre(nombre.toUpperCase());
+        obj.setApellido(apellido.toUpperCase());
+        obj.setDni(dni);
+        container.persistIfNotAlready(obj);
+        return obj;
+    }
+
+    //clientes basicos
+	
 
 	private List<Modelo> modelosList= new ArrayList<Modelo>();
 	  @Collection(
@@ -69,8 +92,8 @@ public class MarcaModeloViewmodel extends AbstractViewModel {
 	    @CollectionLayout(
 	            render = RenderType.EAGERLY
 	    )
-	  public boolean getMarca1() {
-			return MarcaRepositorio.listarTodo().contains(marca);
+	  public List<Marca> getMarca1() {
+			return MarcaRepositorio.listarTodo();
 		}
 //Marca
 	
@@ -93,7 +116,8 @@ public class MarcaModeloViewmodel extends AbstractViewModel {
     )
 	public List<Cliente> getCliente() {
 		return ClienteRepositorio.ultimosClientes().subList(0, 10);//Filtro los resultados para que devuelva solo 10 clientes
-	}
+    	//return ClienteRepositorio.ultimosClientes().contains()
+    }
 	//............
 
 	@Override
