ad766438bfa38acb43feac7352a9cc7677d6d4dd
Armando Bosso <bossoar@gmail.com>
2015-12-03 23:32:14 -0200

    Clase para los reportes

---
+++ dom/src/main/java/domainapp/dom/modules/reportes/EquiposSinRevisar.java
@@ -0,0 +1,27 @@
+package domainapp.dom.modules.reportes;
+
+public class EquiposSinRevisar {
+	
+	// {{ Titulo (property)
+		private String titulo;
+
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(final String titulo) {
+			this.titulo = titulo;
+		}
+		// }}
+		
+		private String falla;
+		
+		 public String getFalla() {
+		        return falla;
+		 }
+		 public void setFalla(final String falla) {
+		        this.falla = falla;
+		 }
+
+}
