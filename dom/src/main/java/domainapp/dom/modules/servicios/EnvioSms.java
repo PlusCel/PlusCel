118725359229e3d2b1520fd3988658fcd982052d
nachocartes <nachocartes@gmail.com>
2015-12-11 20:33:15 -0300

    Mejoro interfaz grafica

---
+++ dom/src/main/java/domainapp/dom/modules/servicios/EnvioSms.java
@@ -0,0 +1,17 @@
+package domainapp.dom.modules.servicios;
+
+import org.apache.isis.applib.annotation.DomainService;
+import org.apache.isis.applib.AbstractFactoryAndRepository;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+@DomainService
+
+public class EnvioSms extends AbstractFactoryAndRepository {
+	
+}
