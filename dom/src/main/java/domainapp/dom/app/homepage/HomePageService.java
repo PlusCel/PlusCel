600c30f7d12d144f3512549d0a5f7795284fda18
Armando Bosso <bossoar@gmail.com>
2015-12-21 09:11:09 -0200

    Agrego licencias

--- dom/src/main/java/domainapp/dom/app/homepage/HomePageService.java
+++ dom/src/main/java/domainapp/dom/app/homepage/HomePageService.java
@@ -1,21 +1,25 @@
 /*
- *  Licensed to the Apache Software Foundation (ASF) under one
- *  or more contributor license agreements.  See the NOTICE file
- *  distributed with this work for additional information
- *  regarding copyright ownership.  The ASF licenses this file
- *  to you under the Apache License, Version 2.0 (the
- *  "License"); you may not use this file except in compliance
- *  with the License.  You may obtain a copy of the License at
+ * This is software for administration and management of mobile equipment repair
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ * Copyright ( C ) 2015 , Pluscel
  *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
+ * This program is free software ; you can redistribute it and / or
+ * Modify it under the terms of the GNU General Public License
+ * As published by the Free Software Foundation ; either version 2
+ * Of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * But WITHOUT ANY WARRANTY; without even the implied warranty
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Boil
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * Along with this program ; if not, write to the Free Software
+ *
+ *
+ * Foundation , Inc. , 51 Franklin Street, Fifth Floor , Boston, MA 02110-1301 , USA .
  */
+
 package domainapp.dom.app.homepage;
 
 import org.apache.isis.applib.DomainObjectContainer;
