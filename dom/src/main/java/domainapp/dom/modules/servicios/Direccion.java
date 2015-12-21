8fbaefb5cd40bfdb2605834b860fdf9664d3ddb1
Armando Bosso <bossoar@gmail.com>
2015-12-21 09:40:02 -0200

    Agrego licencias

--- dom/src/main/java/domainapp/dom/modules/servicios/Direccion.java
+++ dom/src/main/java/domainapp/dom/modules/servicios/Direccion.java
@@ -1,24 +1,24 @@
 /*
- * This is a software made for PlusCel management 
- * 
- * Copyright (C) 2015, PlusCel
- * 
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * This is software for administration and management of mobile equipment repair
+ *
+ * Copyright ( C ) 2015 , Pluscel
+ *
+ * This program is free software ; you can redistribute it and / or
+ * Modify it under the terms of the GNU General Public License
+ * As published by the Free Software Foundation ; either version 2
+ * Of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * But WITHOUT ANY WARRANTY; without even the implied warranty
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. Boil
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * 
- * 
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-*/
+ * Along with this program ; if not, write to the Free Software
+ *
+ *
+ * Foundation , Inc. , 51 Franklin Street, Fifth Floor , Boston, MA 02110-1301 , USA .
+ */
 
 package domainapp.dom.modules.servicios;
 import javax.jdo.annotations.Column;
